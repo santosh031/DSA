14fe151d94b92a43b71c64297224fec81432e970
santosh031 <67217612+santosh031@users.noreply.github.com>
2023-01-22 14:30:24 +0530

    list in different types

--- list.py
+++ list.py
@@ -1,15 +1,18 @@
-''' n= int(input())
+#add elements in list simple
+n= int(input())
 a = list(map(int, input().split()))[:n]
-print(a) '''
+print(a)
 
-
-''' l = int(input())
+#adding elements in list using For Loop
+l = int(input())
 b = []
 for i in range(0,l):
     n = int(input())
     b.append(n)
-print(b) '''
+print(b)
 
+
+#Adding elements in List usin Function
 def li(p):
     l=[]
     for i in range(0,p):
