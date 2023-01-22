2a4ebfe59341731fe4cff87fe5fd4c3fdcd3d296
santosh031 <67217612+santosh031@users.noreply.github.com>
2023-01-22 12:02:36 +0530

    third

---
+++ list.py
@@ -0,0 +1,21 @@
+''' n= int(input())
+a = list(map(int, input().split()))[:n]
+print(a) '''
+
+
+''' l = int(input())
+b = []
+for i in range(0,l):
+    n = int(input())
+    b.append(n)
+print(b) '''
+
+def li(p):
+    l=[]
+    for i in range(0,p):
+        n= int(input().split())
+        l.append(n)
+    print(l)
+
+p = int(input())
+li(p)
